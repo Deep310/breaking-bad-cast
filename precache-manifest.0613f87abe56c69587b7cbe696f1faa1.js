af9859782ca3717b644511be38fb11ef0f59c588
Deep310 <dparekh2019@gmail.com>
2020-07-12 09:10:09 -0400

    Updates

---
+++ precache-manifest.0613f87abe56c69587b7cbe696f1faa1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf37e8ce2263de356d27ce98a41f8eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b84d6b7ae32ef3d20796",
+    "url": "/static/css/main.e4a1d483.chunk.css"
+  },
+  {
+    "revision": "063a7d9a38da3de85d6e",
+    "url": "/static/js/2.813b259a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.813b259a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84d6b7ae32ef3d20796",
+    "url": "/static/js/main.9d7c19b9.chunk.js"
+  },
+  {
+    "revision": "4aad21eb92ea52ff6838",
+    "url": "/static/js/runtime-main.9adee5b9.js"
+  },
+  {
+    "revision": "e9adbff05f725b62a9c7b34dbb88ad12",
+    "url": "/static/media/BreakingBadLogo.e9adbff0.png"
+  },
+  {
+    "revision": "0dcb86e737309e97176808e0f4fe49de",
+    "url": "/static/media/spinner.gif.0dcb86e7.gif"
+  }
+]);
